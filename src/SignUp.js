b32241997f59dfb7c882c676861296c2f60cb1bc
alekyahq <web.alekya.ai@gmail.com>
2021-07-15 00:10:47 +0530

    login static

--- src/SignUp.js
+++ src/SignUp.js
@@ -2,6 +2,24 @@ import React from 'react';
 
 import logo from "./logo.jfif"
 import  { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBCard,MDBIcon, MDBCardBody } from 'mdb-react-ui-kit';
+import {TextField} from  '@material-ui/core/Button';
+
+
+const Input = (props) => {
+  let formattedName = props.name;
+  let inputRef = props.inputRef
+  .toLowerCase()
+  .split(" ")
+  .join("_");
+  
+  return (
+    <div className="custom-input input">      
+      <input type="text" id={inputRef} />
+      <label htmlFor={inputRef}>{formattedName}</label>
+    </div>
+  )
+}
+
 
 function Signup() {
   return (
@@ -15,19 +33,15 @@ function Signup() {
 
       <br /><br />
 
+      <MDBCol>
+        <MDBRow md='8' className='mx-auto'>
+    <div className='card p-4'>
      <MDBRow >
      <MDBCol md="2">
-
      </MDBCol>
         <MDBCol md="4" className='' style={{"margin-top" : "0%"}}>
-
-       
           <div style={{"width" : "100%"}} className="mx-auto  " >
-
-          
-           
             <MDBCardBody className='p-0 rounded'>
-          
             <div className=' ' style={{ "object-fit" : "scale-down"}}>
             <h1 className='text-center text-muted'>Register for ATS</h1>
             <p className="text-center">Get Your Free Trial for 30 Days!!</p>
@@ -35,82 +49,13 @@ function Signup() {
 
             </div>
               <form className='px-4'>
-                
-                <div className="grey-text px-4">
-                  <MDBInput
-                    label="Your name"
-                    icon="user"
-                    group
-                    type="text"
-                    validate
-                    className = "my-4"
-                    error="wrong"
-                    success="right"
-                  />
-                  <MDBInput
-                    label="Company name"
-                    icon="user"
-                    group
-                    type="text"
-                    validate
-                    className = "my-4"
-                    error="wrong"
-                    success="right"
-                  />
-                  <MDBInput
-                    label="Your email"
-                    icon="envelope"
-                    group
-                    type="email"
-                    validate
-                    error="wrong"
-                    className = "my-4"
-                    success="right"
-                  />
-                   <MDBInput
-                    label="Phone"
-                    icon="Lock"
-                    group
-                    type="email"
-                    validate
-                    error="wrong"
-                    className = "my-4"
-                    success="right"
-                  />
+              <Input name="Username" inputRef="Username" />
+              <Input name="Company Name" inputRef="Company Name" />
+              
+              <Input name="Phone" inputRef="Phone" />
 
-                  <MDBInput
-                    label="Location"
-                    icon="Lock"
-                    group
-                    type="email"
-                    validate
-                    error="wrong"
-                    className = "my-4"
-                    success="right"
-                  />
-
-                  <MDBInput
-                    label="Username"
-                    icon="Lock"
-                    group
-                    type="email"
-                    validate
-                    error="wrong"
-                    className = "my-4"
-                    success="right"
-                  />
-                  <MDBInput
-                    label="password"
-                    icon="Lock"
-                    group
-                    type="email"
-                    validate
-                    error="wrong"
-                    className = "my-4"
-                    success="right"
-                  />
-                 
-                </div>
+              <Input name="Email" inputRef="user email" />
+              <Input name="Password" inputRef="Password" />
 
 
                 <div className='px-4'>
@@ -139,6 +84,12 @@ function Signup() {
                   </MDBBtn>
                 </div>
 
+                <div className="text-center">
+                  <MDBBtn color="cyan" className='mt-3 btn btn-primary' type="submit" style={{"width" : "82%"}}>
+                  <MDBIcon fab icon="linkedin mx-2" />     Register with Linkedin
+                  </MDBBtn>
+                </div>
+
                 <br /><br />
 
             </MDBCardBody>
@@ -152,6 +103,9 @@ function Signup() {
 
         </MDBCol>
       </MDBRow>
+    </div>
+  </MDBRow>
+  </MDBCol>
     </MDBContainer>
   );
 }
